26923143809adbcd697e57384546addc3e645b1a
viviankrieck <vivian_pereira@outlook.com.br>
2025-08-08 07:46:55 -0300

    feat: Update project configuration and add login functionality
    - Updated package.json to include project metadata and new scripts for development and serving.
    - Added logo images for branding in the public/assets/img directory.
    - Created app.css with Tailwind CSS imports and custom styles for the application.
    - Implemented a login view in resources/views/livewire/auth/login.blade.php.
    - Modified welcome.blade.php to serve as a layout for the application and updated the title and footer.
    - Changed the default route to point to the new login view.
    - Added Tailwind CSS configuration in tailwind.config.js.
    - Updated vite.config.js to include refresh paths for Livewire components.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,25 +5,32 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Livewire</title>
+        <title>VK Beauty Scheduler</title>
+
+        <meta name="description" content="VK Beauty Scheduler - Agendamento de Beleza" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="application-name" content="VK Beauty Scheduler" />
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
         <style>
-            @import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Roboto+Slab:wght@100..900&display=swap');
+            @import url('https://fonts.googleapis.com/css2?family=Loira:ital,wght@0,400..700;1,400..700&family=Roboto+Slab:wght@100..900&display=swap');
         </style>
     </head>
 
-    <body>
-        <div class="text-2xl">Bem-vindo ao Livewire</div>
-        <div class="text-sm">
-            Este é um exemplo simples de componente do Livewire.
+    <body class="bg-page">
+        <div class="container">
+            <header class="text-2xl">Bem-vindo ao VK Beauty Scheduler</header>
+            <main>
+                @yield('content')
+            </main>
         </div>
-        {{--
-            <livewire:counter />
-            <livewire:message />
-        --}}
-        <livewire:calculator />
-
+        <footer>
+            <div class="container mx-auto py-4 inset-x-0 bottom-0 mt-4">
+                <p class="text-center text-sm text-gray-600">
+                    &copy; {{ date('Y') }} VK Beauty Scheduler. Todos os direitos reservados.
+                </p>
+            </div>
+        </footer>
         @livewireScripts
     </body>
 
